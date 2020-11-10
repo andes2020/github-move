0769ee2389465ea0be51245be6a6ae8ffb382f50
Andes <andes.lam@maersk.com>
2020-11-10 09:54:46 +0000

    Add logic on winning the tic-tac-toe

--- react-eacf-app/src/index.js
+++ react-eacf-app/src/index.js
@@ -9,6 +9,26 @@ function Square(props) {
       </button>
     );
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
   
   class Board extends React.Component {
     //This create a share state in parent board component
@@ -23,6 +43,10 @@ function Square(props) {
     // Implement handleclick()
     handleClick(i) {
       const squares = this.state.squares.slice();
+      // Check if a win exist or square filled
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       //Update existing array of 9 squares
       //Flip xIsNext for next turn
@@ -41,7 +65,14 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner
+      }else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
